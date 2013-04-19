4b73f3178c5ef827543ae6ce7a6589e0211c063e
yijing bai <everatnight@gmail.com>
2013-04-19 15:39:45 -0700

    fix the bug but stil very slow

--- sgemm-small.c
+++ sgemm-small.c
@@ -6,15 +6,14 @@
 void sgemm(int m, int n, int d, float *A, float *C) {
     int i, j = 0;
     for (j = 0; j < n; j++) {
-        for (i = 0; i < i<n-n%40; i += 40) {
+        for (i = 0; i < n-n%40; i += 40) {
             __m128 c[10];
             for (int r = 0; r < 10; r++)
                 c[r] = _mm_setzero_ps();
-            float *p = A+i;
             for (int k = 0; k < m; k++) {
                 __m128 b = _mm_load1_ps(A+j*(n+1)+k*(n));
                 for (int r = 0; r < 10; r++) {
-                    c[r] = _mm_add_ps(c[r], _mm_mul_ps(_mm_loadu_ps(p + n*k + r*4), b));
+                    c[r] = _mm_add_ps(c[r], _mm_mul_ps(_mm_loadu_ps(A+i+n*k+r*4), b));
                 }
             }
 
