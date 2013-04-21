10b191369730fbb563d1306ebff43c441264a352
yijing bai <everatnight@gmail.com>
2013-04-21 12:32:49 -0700

    delete the comment

--- sgemm-small.c
+++ sgemm-small.c
@@ -59,21 +59,13 @@ void sgemm(int m, int n, int d, float *A, float *C) {
             }
             for (int k = 0; k < m; k++) {
                 __m128 b = _mm_load1_ps(A+j*(n+1)+k*(n));
-                for (int r = 0; r < 5; r+=5) {
+                for (int r = 0; r < 5; r++) {
                     c[r] = _mm_add_ps(c[r], _mm_mul_ps(_mm_loadu_ps(A+i+n*k+r*4), b));
-                    c[r+1] = _mm_add_ps(c[r+1], _mm_mul_ps(_mm_loadu_ps(A+i+n*k+(r+1)*4), b));
-                    c[r+2] = _mm_add_ps(c[r+2], _mm_mul_ps(_mm_loadu_ps(A+i+n*k+(r+2)*4), b));
-                    c[r+3] = _mm_add_ps(c[r+3], _mm_mul_ps(_mm_loadu_ps(A+i+n*k+(r+3)*4), b));
-                    c[r+4] = _mm_add_ps(c[r+4], _mm_mul_ps(_mm_loadu_ps(A+i+n*k+(r+4)*4), b));
                 }
             }
 
-            for (int r = 0; r < 5; r+=5) {
+            for (int r = 0; r < 5; r++) {
                 _mm_storeu_ps(C+i+r*4+j*n, c[r]);
-                _mm_storeu_ps(C+i+(r+1)*4+j*n, c[r+1]);
-                _mm_storeu_ps(C+i+(r+2)*4+j*n, c[r+2]);
-                _mm_storeu_ps(C+i+(r+3)*4+j*n, c[r+3]);
-                _mm_storeu_ps(C+i+(r+4)*4+j*n, c[r+4]);
             }
         }
 
@@ -94,8 +86,10 @@ void sgemm(int m, int n, int d, float *A, float *C) {
                 }
             }
 
-            for (int r = 0; r < 3; r++) {
+            for (int r = 0; r < 3; r+=3) {
                 _mm_storeu_ps(C+i+r*4+j*n, c[r]);
+                _mm_storeu_ps(C+i+(r+1)*4+j*n, c[r+1]);
+                _mm_storeu_ps(C+i+(r+2)*4+j*n, c[r+2]);
             }
         }
 
@@ -115,39 +109,6 @@ void sgemm(int m, int n, int d, float *A, float *C) {
             }
         }
         
-        /*
-        for (i = n_20; i < n_16; i += 16) {
-            __m128 c[2];
-            for (int r = 0; r < 2; r++)
-                c[r] = _mm_setzero_ps();
-            for (int k = 0; k < m; k++) {
-                __m128 b = _mm_load1_ps(A+j*(n+1)+k*(n));
-                for (int r = 0; r < 2; r++) {
-                    c[r] = _mm_add_ps(c[r], _mm_mul_ps(_mm_loadu_ps(A+i+n*k+r*4), b));
-                }
-            }
-
-            for (int r = 0; r < 2; r++) {
-                _mm_storeu_ps(C+i+r*4+j*n, c[r]);
-            }   
-        }
-        
-        for (i = n-n%8; i < n-n%4; i += 4) {
-            __m128 c[1];
-            for (int r = 0; r < 1; r++)
-                c[r] = _mm_setzero_ps();
-            for (int k = 0; k < m; k++) {
-                __m128 b = _mm_load1_ps(A+j*(n+1)+k*(n));
-                for (int r = 0; r < 1; r++) {
-                    c[r] = _mm_add_ps(c[r], _mm_mul_ps(_mm_loadu_ps(A+i+n*k+r*4), b));
-                }
-            }
-
-            for (int r = 0; r < 1; r++) {
-                _mm_storeu_ps(C+i+r*4+j*n, c[r]);
-            }   
-        }
-        */
         for (i = n_4; i < n; i++) {
             for (int k = 0; k < m; k++) {
                 C[i+j*n] += A[i+k*(n)] * A[j*(n+1)+k*(n)];
